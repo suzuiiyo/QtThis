9bf7a682f0af48c39f8579584e1186eb2b91a70c
Suzuiiyo <she7ter@gmail.com>
2022-08-23 20:57:46 +0800

    add all

---
+++ build/compareWave/moc_mainwindow.cpp
@@ -0,0 +1,301 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.9.6)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../compareWave/mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.9.6. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[35];
+    char stringdata0[448];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 16), // "SigReadFileBegin"
+QT_MOC_LITERAL(2, 28, 0), // ""
+QT_MOC_LITERAL(3, 29, 5), // "a_num"
+QT_MOC_LITERAL(4, 35, 18), // "SigReadFilePercent"
+QT_MOC_LITERAL(5, 54, 10), // "a_nPercent"
+QT_MOC_LITERAL(6, 65, 19), // "SigReadFileFinished"
+QT_MOC_LITERAL(7, 85, 18), // "SigReadFileSegment"
+QT_MOC_LITERAL(8, 104, 12), // "doSelectFont"
+QT_MOC_LITERAL(9, 117, 13), // "doSelectFont2"
+QT_MOC_LITERAL(10, 131, 8), // "showItem"
+QT_MOC_LITERAL(11, 140, 8), // "itemInfo"
+QT_MOC_LITERAL(12, 149, 2), // "on"
+QT_MOC_LITERAL(13, 152, 12), // "showItemNext"
+QT_MOC_LITERAL(14, 165, 11), // "showItemSam"
+QT_MOC_LITERAL(15, 177, 15), // "showItemSamNext"
+QT_MOC_LITERAL(16, 193, 14), // "doSelectSwitch"
+QT_MOC_LITERAL(17, 208, 15), // "doSelectSwitch2"
+QT_MOC_LITERAL(18, 224, 8), // "testslot"
+QT_MOC_LITERAL(19, 233, 9), // "testslot2"
+QT_MOC_LITERAL(20, 243, 17), // "slotReadFileBegin"
+QT_MOC_LITERAL(21, 261, 19), // "slotReadFilePercent"
+QT_MOC_LITERAL(22, 281, 20), // "slotReadFileFinished"
+QT_MOC_LITERAL(23, 302, 19), // "slotReadFileSegment"
+QT_MOC_LITERAL(24, 322, 11), // "openFileOne"
+QT_MOC_LITERAL(25, 334, 11), // "openFileTwo"
+QT_MOC_LITERAL(26, 346, 11), // "showWaveOne"
+QT_MOC_LITERAL(27, 358, 11), // "showWaveTwo"
+QT_MOC_LITERAL(28, 370, 11), // "compareWave"
+QT_MOC_LITERAL(29, 382, 9), // "alignWave"
+QT_MOC_LITERAL(30, 392, 16), // "switchConclusion"
+QT_MOC_LITERAL(31, 409, 8), // "onZoomer"
+QT_MOC_LITERAL(32, 418, 9), // "onZoomer2"
+QT_MOC_LITERAL(33, 428, 9), // "onZoomer3"
+QT_MOC_LITERAL(34, 438, 9) // "onZoomer4"
+
+    },
+    "MainWindow\0SigReadFileBegin\0\0a_num\0"
+    "SigReadFilePercent\0a_nPercent\0"
+    "SigReadFileFinished\0SigReadFileSegment\0"
+    "doSelectFont\0doSelectFont2\0showItem\0"
+    "itemInfo\0on\0showItemNext\0showItemSam\0"
+    "showItemSamNext\0doSelectSwitch\0"
+    "doSelectSwitch2\0testslot\0testslot2\0"
+    "slotReadFileBegin\0slotReadFilePercent\0"
+    "slotReadFileFinished\0slotReadFileSegment\0"
+    "openFileOne\0openFileTwo\0showWaveOne\0"
+    "showWaveTwo\0compareWave\0alignWave\0"
+    "switchConclusion\0onZoomer\0onZoomer2\0"
+    "onZoomer3\0onZoomer4"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      29,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       4,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    1,  159,    2, 0x06 /* Public */,
+       4,    1,  162,    2, 0x06 /* Public */,
+       6,    0,  165,    2, 0x06 /* Public */,
+       7,    0,  166,    2, 0x06 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags
+       8,    1,  167,    2, 0x08 /* Private */,
+       9,    1,  170,    2, 0x08 /* Private */,
+      10,    2,  173,    2, 0x08 /* Private */,
+      13,    2,  178,    2, 0x08 /* Private */,
+      14,    2,  183,    2, 0x08 /* Private */,
+      15,    2,  188,    2, 0x08 /* Private */,
+      16,    1,  193,    2, 0x08 /* Private */,
+      17,    1,  196,    2, 0x08 /* Private */,
+      18,    1,  199,    2, 0x08 /* Private */,
+      19,    1,  202,    2, 0x08 /* Private */,
+      20,    1,  205,    2, 0x08 /* Private */,
+      21,    1,  208,    2, 0x08 /* Private */,
+      22,    0,  211,    2, 0x08 /* Private */,
+      23,    0,  212,    2, 0x08 /* Private */,
+      24,    0,  213,    2, 0x08 /* Private */,
+      25,    0,  214,    2, 0x08 /* Private */,
+      26,    0,  215,    2, 0x08 /* Private */,
+      27,    0,  216,    2, 0x08 /* Private */,
+      28,    0,  217,    2, 0x08 /* Private */,
+      29,    0,  218,    2, 0x08 /* Private */,
+      30,    0,  219,    2, 0x08 /* Private */,
+      31,    1,  220,    2, 0x08 /* Private */,
+      32,    1,  223,    2, 0x08 /* Private */,
+      33,    1,  226,    2, 0x08 /* Private */,
+      34,    1,  229,    2, 0x08 /* Private */,
+
+ // signals: parameters
+    QMetaType::Void, QMetaType::Int,    3,
+    QMetaType::Void, QMetaType::Int,    5,
+    QMetaType::Void,
+    QMetaType::Void,
+
+ // slots: parameters
+    QMetaType::Void, QMetaType::Int,    2,
+    QMetaType::Void, QMetaType::Int,    2,
+    QMetaType::Void, QMetaType::QVariant, QMetaType::Bool,   11,   12,
+    QMetaType::Void, QMetaType::QVariant, QMetaType::Bool,   11,   12,
+    QMetaType::Void, QMetaType::QVariant, QMetaType::Bool,   11,   12,
+    QMetaType::Void, QMetaType::QVariant, QMetaType::Bool,   11,   12,
+    QMetaType::Void, QMetaType::Int,    2,
+    QMetaType::Void, QMetaType::Int,    2,
+    QMetaType::Void, QMetaType::QPolygon,    2,
+    QMetaType::Void, QMetaType::QPointF,    2,
+    QMetaType::Void, QMetaType::Int,    3,
+    QMetaType::Void, QMetaType::Int,    5,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QRectF,    2,
+    QMetaType::Void, QMetaType::QRectF,    2,
+    QMetaType::Void, QMetaType::QRectF,    2,
+    QMetaType::Void, QMetaType::QRectF,    2,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        MainWindow *_t = static_cast<MainWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->SigReadFileBegin((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 1: _t->SigReadFilePercent((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 2: _t->SigReadFileFinished(); break;
+        case 3: _t->SigReadFileSegment(); break;
+        case 4: _t->doSelectFont((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 5: _t->doSelectFont2((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 6: _t->showItem((*reinterpret_cast< const QVariant(*)>(_a[1])),(*reinterpret_cast< bool(*)>(_a[2]))); break;
+        case 7: _t->showItemNext((*reinterpret_cast< const QVariant(*)>(_a[1])),(*reinterpret_cast< bool(*)>(_a[2]))); break;
+        case 8: _t->showItemSam((*reinterpret_cast< const QVariant(*)>(_a[1])),(*reinterpret_cast< bool(*)>(_a[2]))); break;
+        case 9: _t->showItemSamNext((*reinterpret_cast< const QVariant(*)>(_a[1])),(*reinterpret_cast< bool(*)>(_a[2]))); break;
+        case 10: _t->doSelectSwitch((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 11: _t->doSelectSwitch2((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 12: _t->testslot((*reinterpret_cast< const QPolygon(*)>(_a[1]))); break;
+        case 13: _t->testslot2((*reinterpret_cast< const QPointF(*)>(_a[1]))); break;
+        case 14: _t->slotReadFileBegin((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 15: _t->slotReadFilePercent((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 16: _t->slotReadFileFinished(); break;
+        case 17: _t->slotReadFileSegment(); break;
+        case 18: _t->openFileOne(); break;
+        case 19: _t->openFileTwo(); break;
+        case 20: _t->showWaveOne(); break;
+        case 21: _t->showWaveTwo(); break;
+        case 22: _t->compareWave(); break;
+        case 23: _t->alignWave(); break;
+        case 24: _t->switchConclusion(); break;
+        case 25: _t->onZoomer((*reinterpret_cast< const QRectF(*)>(_a[1]))); break;
+        case 26: _t->onZoomer2((*reinterpret_cast< const QRectF(*)>(_a[1]))); break;
+        case 27: _t->onZoomer3((*reinterpret_cast< const QRectF(*)>(_a[1]))); break;
+        case 28: _t->onZoomer4((*reinterpret_cast< const QRectF(*)>(_a[1]))); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            typedef void (MainWindow::*_t)(int );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&MainWindow::SigReadFileBegin)) {
+                *result = 0;
+                return;
+            }
+        }
+        {
+            typedef void (MainWindow::*_t)(int );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&MainWindow::SigReadFilePercent)) {
+                *result = 1;
+                return;
+            }
+        }
+        {
+            typedef void (MainWindow::*_t)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&MainWindow::SigReadFileFinished)) {
+                *result = 2;
+                return;
+            }
+        }
+        {
+            typedef void (MainWindow::*_t)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&MainWindow::SigReadFileSegment)) {
+                *result = 3;
+                return;
+            }
+        }
+    }
+}
+
+const QMetaObject MainWindow::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_MainWindow.data,
+      qt_meta_data_MainWindow,  qt_static_metacall, nullptr, nullptr}
+};
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 29)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 29;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 29)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 29;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void MainWindow::SigReadFileBegin(int _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
+}
+
+// SIGNAL 1
+void MainWindow::SigReadFilePercent(int _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 1, _a);
+}
+
+// SIGNAL 2
+void MainWindow::SigReadFileFinished()
+{
+    QMetaObject::activate(this, &staticMetaObject, 2, nullptr);
+}
+
+// SIGNAL 3
+void MainWindow::SigReadFileSegment()
+{
+    QMetaObject::activate(this, &staticMetaObject, 3, nullptr);
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
